ca116b930323c4fd411db4082e2d9607218eae0c
Rui Barreto <ruibarreto07@outlook.com>
2024-04-28 10:23:03 +0100

    weather app updated

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var button = document.querySelector('.submit');
 var latitudeElement = document.querySelector('.latitude');
 var forecastElement = document.querySelector('.forecast');
 
-//Exibe a temperatura, descrição, icon e o nome da cidade que metemos no input text. Também foi convertido Kelvin para Celsius e a linguagem para pt
 button.addEventListener('click', function (name) {
   fetch('http://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&APPID=0503813858c019c4d5fce902682d15fa&lang=pt')
     .then(response => response.json())
@@ -25,19 +24,13 @@ button.addEventListener('click', function (name) {
 
       input.value = "";
 
-      /* O nome da cidade que foi inserido no input text será convertido para a latitude e longitude para a api do forecast funcionar, visto que o 
-      OpenWeatherMap Forecast só mostra as temperaturas se tivermos a latitude e a longitude*/
       fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + encodeURIComponent(nameValue) + '&limit=5&appid=0503813858c019c4d5fce902682d15fa&lan=pt')
         .then(response => response.json())
         .then(geoData => {
           if (geoData.length > 0) {
             var latitude = geoData[0].lat;
             var longitude = geoData[0].lon;
-            console.log("Latitude:", latitude);
-            console.log("Longitude:", longitude);
 
-            /* Aqui chamamos a api do forecast para exibir a temperatura da cidade que metermos para as próximas horas. Chamamos as variáveis latitude e longitude
-            para que a api forecast funcione corretamente*/ 
             fetch('http://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=0503813858c019c4d5fce902682d15fa&lang=pt')
               .then(response => response.json())
               .then(forecastData => {
